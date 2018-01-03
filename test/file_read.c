6d2f01b1acff73c438bfdf8614a395092b3a9f31
유근명 <gmyou@onnuridmc.com>
2018-01-03 18:04:33 +0900

    읽어서 나누기

---
+++ test/file_read.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    FILE *pFile = NULL;
+    FILE *oFile = NULL;
+    char fileName[] = "out.csv";
+
+	pFile = fopen( "test.txt", "r" );
+	if( pFile != NULL )
+	{
+		char strTemp[255];
+		char *pStr;
+        int maxLine = 4;
+        int i=0, j=0;
+
+		while( !feof( pFile ) )
+		{
+            if ( i >= maxLine ) {
+                i = 0;
+                j += 1;
+            }
+
+			pStr = fgets( strTemp, sizeof(strTemp), pFile );
+            printf ( "%s", pStr );
+
+            sprintf(fileName, "%d.csv", j);
+
+            oFile = fopen( fileName, "a" );
+            fprintf(oFile, "%s", pStr);
+            i += 1;
+		}
+		fclose( pFile );
+	}
+	else
+	{
+		//에러 처리
+	}
+
+	return 0;
+}
