56aff3e1243a9c9102437036074dc588d6a34aa0
유근명 <gmyou71@gmail.com>
2019-07-31 02:26:52 +0900

    strcpy

--- test/string/c_str.cpp
+++ test/string/c_str.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
 int main()
 {
     string cppstyle = "Yes, I am.";
 
-    const char* cstyle = NULL;
+    // const char* cstyle = NULL;
+    char* cstyle = new char [cppstyle.size() + 1];
 
-    cstyle = cppstyle.c_str();
+    // cstyle = cppstyle.c_str();
+    strcpy( cstyle, cppstyle.c_str() );
+
+    cstyle[0] = 'Z';
 
     cout << "cstyle = " << cstyle << "\n";
     cout << "cppstyle = " << cppstyle << "\n";
+
+    delete[] cstyle;
+    cstyle = NULL;
 
     return 0;
 }
