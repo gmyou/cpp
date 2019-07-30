07b9dcd5e8009992b6eea5385fb2cb69915d6af5
유근명 <gmyou71@gmail.com>
2019-07-31 02:31:49 +0900

    add input test

---
+++ test/string/input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char cs[10];
+    string cpps;
+
+    cin >> cs;
+    cin >> cpps;
+
+    cout << "cs = " << cs << "\n";
+    cout << "cpps = " << cpps << "\n";
+
+    return 0;
+}
